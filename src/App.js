87d2c66e52e67804a7f23b69365421b031d71f21
hintonbradley <hintonbradley@gmail.com>
2019-09-10 22:24:29 -0700

    updated jquery

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Nav from './Components/Nav';
 import Home from './Components/Home';
 import Questions from './Components/Questions';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 // import 'bootstrap/dist/css/bootstrap-theme.min.css' // optional
-import 'jquery/dist/jquery.min.js'
-import 'bootstrap/dist/js/bootstrap.min.js'
+// import 'jquery/dist/jquery.min.js'
+// import 'bootstrap/dist/js/bootstrap.min.js'
 import './App.css';
 
 function App() {
